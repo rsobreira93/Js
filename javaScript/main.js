91f9555d10107fc3c39525b5b6a0e5c243a1e6c4
Romulo Sobreira <romurevoltado@gmail.com>
2020-10-27 09:34:20 -0300

    concatenação de strings

--- javaScript/main.js
+++ javaScript/main.js
@@ -1,5 +1,49 @@
-var nome = "Romulo Sobreira";
-var idade = 27;
+/*
+var d = new Date();
+alert(d.getMonth()+1);
+*/
+
+/*
+for(count = 0; count <= 5; count++){
+    console.log(count);
+}
+*/
+/*
+var count = 0;
+while(count < 5){
+    console.log(count);
+    count ++;
+}
+*/
+
+/*var idade = prompt("Qual sua idade:? ");
+var idade = 18;
+if (idade >= 18){
+    alert("Maior de idade");
+}else{
+    alert("Menor de idade");
+}*/
+
+
+/*var frutas = [{nome:"maça", cor:"vermelha"}, {nome:"uva", cor:"roxa"}]
+console.log(frutas);*/
+
+/*var fruta = {nome:"maça", cor:"vermelha"}
+console.log(fruta.nome);*/
+
+//var lista =["maça", "pêra", "laranja"];
+//lista.push("uva"); adicona elemento  na lista
+//lista.pop(); //remove o ultimo elementoda lista
+
+
+//console.log(lista[1]);
+//console.log(lista.length); para ver o tamanho da lista
+//console.log(lista.reverse()); //para imprimir ao contrario
+//console.log(lista.toString());
+//console.log(lista.join(" |"));
+
+//var nome = "Romulo Sobreira";
+/*var idade = 27;
 var idade2 = 10;
 var frase = "Japão é o melhor time do mundo"
 //alert(nome + " tem " + idade + " anos") ;
@@ -7,4 +51,4 @@ var frase = "Japão é o melhor time do mundo"
 console.log(nome);
 console.log(idade);
 //console.log(frase.replace("Japão","Brasil"));
-console.log(frase.toLocaleLowerCase());
+console.log(frase.toLocaleLowerCase());*/

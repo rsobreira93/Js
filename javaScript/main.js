2a5b28de358cf3f047f84fb654e10eba39b70acf
Romulo Sobreira <romurevoltado@gmail.com>
2020-10-27 08:23:32 -0300

    concatenação de strings

---
+++ javaScript/main.js
@@ -0,0 +1,10 @@
+var nome = "Romulo Sobreira";
+var idade = 27;
+var idade2 = 10;
+var frase = "Japão é o melhor time do mundo"
+//alert(nome + " tem " + idade + " anos") ;
+//alert(idade + idade2);
+console.log(nome);
+console.log(idade);
+//console.log(frase.replace("Japão","Brasil"));
+console.log(frase.toLocaleLowerCase());
